60f85c239c2c34aa76e0a427a0d6663f17fc62d5
24eg105b58-ship-it <24eg105b58@anurag.edu.in>
2026-07-17 10:40:58 +0530

    Add server/routes/productRoutes.js

---
+++ server/routes/productRoutes.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const router = express.Router();
+const {
+  getProducts,
+  getProductById,
+  createProduct,
+  updateProduct,
+  deleteProduct,
+  createProductReview,
+} = require('../controllers/productController');
+const { protect, admin } = require('../middleware/authMiddleware');
+const upload = require('../middleware/uploadMiddleware');
+
+router.route('/')
+  .get(getProducts)
+  .post(protect, admin, upload.single('image'), createProduct);
+
+router.route('/:id')
+  .get(getProductById)
+  .put(protect, admin, upload.single('image'), updateProduct)
+  .delete(protect, admin, deleteProduct);
+
+router.route('/:id/review')
+  .post(protect, createProductReview);
+
+module.exports = router;
